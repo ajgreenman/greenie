27e5c8277ba323aa020c1134c305c43ce2d855e4
Andrew Greenman <A.J.Greenman@gmail.com>
2026-02-15 16:51:45 -0500

    unused text style

--- lib/app/core/theme/text_styles.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Shared text styles for the app.
-/// Use these for consistent typography; extend as needed.
-class GreenieTextStyles {
-  GreenieTextStyles._();
-}
